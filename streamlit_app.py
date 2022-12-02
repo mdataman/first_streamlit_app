7c855268d1cc14255ac00922ee55c1e8d9f33ff6
mdataman <119706645+mdataman@users.noreply.github.com>
2022-12-02 17:07:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 
 streamlit.title("My Parents Healthy New Diner")
 
-streamlit.header("Breakfast Favorites")
-streamlit.text(🥣 "Omega 3 & Blueberry Oatmeal")
-streamlit.text(🥗 "Kale, Spinach & Rocket Smoothie")
-streamlit.text(🐔 "Hard-Boiled Free-Range Egg")
-streamlit.text(🥑 🍞 "Avacado Toast")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 🍞 Avacado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
